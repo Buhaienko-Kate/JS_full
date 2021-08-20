c9e0aca2d72d5ce80982a80032a5e61d41383237
Buhaienko-Kate <buguzia89@gmail.com>
2021-08-20 12:59:53 +0300

    added task2 lesson13

---
+++ Lesson13/task2/calculator.js
@@ -0,0 +1,13 @@
+export const getSquareArray = arr => {
+  return arr.map(el => el * el);
+};
+
+export const getOddNumbers = arr => {
+  return arr.filter(el => {
+    if (el % 2 === 1) {
+      return el;
+    }
+  });
+};
+
+export default (a, b) => a + b;
---
+++ Lesson13/task2/calculator.test.js
@@ -0,0 +1,16 @@
+import getSum, { getSquareArray, getOddNumbers } from './calculator.js';
+
+it('should get squared numbers', () => {
+  let result = getSquareArray([1, 2, 3, 4]);
+  expect(result).toEqual([1, 4, 9, 16]);
+});
+
+it('should keep odd numbers only', () => {
+  let result = getOddNumbers([1, 2, 3, 4]);
+  expect(result).toEqual([1, 3]);
+});
+
+it('should get sum of numbers', () => {
+  let result = getSum(1, 2);
+  expect(result).toEqual(3);
+});
